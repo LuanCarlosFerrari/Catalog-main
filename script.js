fee1fe83cb5478fdaab09ff4295795ff91f1af19
Luan Carlos Ferrari <luan.carlos.ferrari@gmail.com>
2024-10-30 19:36:20 -0300

    badge link if have discount

--- script.js
+++ script.js
@@ -6,18 +6,22 @@ document.querySelectorAll('.produtos__item').forEach(item => {
     const precoOriginalElement = item.querySelector('.preco-original');
     const precoDescontoElement = item.querySelector('.preco-desconto');
     const descontoElement = item.querySelector('.desconto');
+    const badgeElement = item.querySelector('.badge');
 
     const precoOriginal = parseFloat(precoOriginalElement.textContent.replace('R$', '').replace(',', '.'));
     const porcentagemDesconto = parseFloat(descontoElement.textContent.replace('-', '').replace('%', ''));
 
-    const precoComDesconto = calcularDesconto(precoOriginal, porcentagemDesconto);
-
-    precoDescontoElement.textContent = `R$ ${precoComDesconto.toFixed(2).replace('.', ',')}`;
-
     if (porcentagemDesconto > 0) {
         const precoComDesconto = calcularDesconto(precoOriginal, porcentagemDesconto);
         precoDescontoElement.textContent = `R$ ${precoComDesconto.toFixed(2).replace('.', ',')}`;
+        precoOriginalElement.textContent = `R$ ${precoOriginal.toFixed(2).replace('.', ',')}`;
+        descontoElement.textContent = `${porcentagemDesconto}%`;
+        badgeElement.textContent = `-${porcentagemDesconto}%`;
+        badgeElement.style.display = 'block'; // Exibe o badge
     } else {
-        precoDescontoElement.textContent = '';
+        precoDescontoElement.textContent = `R$ ${precoOriginal.toFixed(2).replace('.', ',')}`;
+        precoOriginalElement.textContent = '';
+        descontoElement.textContent = '';
+        badgeElement.style.display = 'none'; // Oculta o badge
     }
 });
